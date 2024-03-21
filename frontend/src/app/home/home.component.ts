72d2233ddb4c22145e87f7bdde0fbc11e77057a8
goldspide <raoulbonsso@gmail.com>
2024-03-21 19:59:31 +0100

    login and passworforgot is okay

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { SignupComponent } from '../signup/signup.component';
+import { ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-home',
@@ -18,6 +20,17 @@ signUpAction(){
   const dialogConfig = new MatDialogConfig();
   dialogConfig.width = "550px";
   this.dialog.open(SignupComponent,dialogConfig)
+}
+forgotPasswordAction(){
+  const dialogConfig = new MatDialogConfig();
+  dialogConfig.width = "550px";
+  this.dialog.open(ForgotPasswordComponent,dialogConfig)
+}
+loginAction(){
+  const dialogConfig = new MatDialogConfig();
+  dialogConfig.width = "550px";
+  this.dialog.open(LoginComponent,dialogConfig)
+
 }
 
 }
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -12,9 +12,20 @@ export class UserService {
 
     signUp(data:any){
       console.log("on entre ici")
-        return this.httpClient.post(this.url+"user/signup", data,{
-            headers: new HttpHeaders().set('Content-type', 'application/json')
+        return this.httpClient.post(this.url+"user/signup/",data,{
+            headers: new HttpHeaders().set('Content-type', "application/json")
             
+        })
+    }
+    forgotPassword(data:any){
+        return this.httpClient.post(this.url+"user/forvvgotPassword/",data,{
+            headers: new HttpHeaders().set('Content-Type', "application/json")
+        })
+    }
+
+    login(data:any){
+        return this.httpClient.post(this.url+"user/login/",data,{
+            headers: new HttpHeaders().set('Content-Type', "application/json")
         })
     }
 }
