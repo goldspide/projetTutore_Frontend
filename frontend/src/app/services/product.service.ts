1d0d08886d7b1ad43865c673532781d707bb37c3
goldspide <raoulbonsso@gmail.com>
2024-03-26 12:26:55 +0100

    product ok

---
+++ frontend/src/app/services/product.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  add(data:any){
+    return this.httpClient.post("http://127.0.0.1:3000/product/add",data,{
+      headers: new HttpHeaders().set('Content-type', "application/json")
+    })
+  }
+  update(data:any){
+    return this.httpClient.patch("http://127.0.0.1:3000/product/update",data,{
+      headers: new HttpHeaders().set('Content-type', "application/json")
+    })
+  }
+  getproducts(){
+    return this.httpClient.get("http://127.0.0.1:3000/product/get",{
+      headers: new HttpHeaders().set('Content-type', "application/json")
+    })
+  }
+
+  updateStatus(data:any){
+    return this.httpClient.patch("http://127.0.0.1:3000/product/updateStatus",data,{
+      headers: new HttpHeaders().set('Content-type', "application/json")
+    })
+  }
+
+  delete(id:any){
+    return this.httpClient.delete("http://127.0.0.1:3000/product/delete/"+id,{
+      headers: new HttpHeaders().set('Content-type', "application/json")
+    })
+  }
+
+}
